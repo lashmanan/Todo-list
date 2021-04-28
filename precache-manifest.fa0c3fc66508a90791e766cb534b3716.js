aad8bdd1483cb03b0dcb1972fad71bc7ca11c3a1
lashmanan <kmlakshmanan16@gmail.com>
2021-04-28 11:31:52 +0530

    Updates

---
+++ precache-manifest.fa0c3fc66508a90791e766cb534b3716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e621b79d99094907bcb9b23db8cdcba",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "11a15c2391bb3f9ed98d",
+    "url": "/Todo-list/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c2d6c094d54c4ac94364",
+    "url": "/Todo-list/static/css/main.5820d271.chunk.css"
+  },
+  {
+    "revision": "11a15c2391bb3f9ed98d",
+    "url": "/Todo-list/static/js/2.ca743ca3.chunk.js"
+  },
+  {
+    "revision": "c2d6c094d54c4ac94364",
+    "url": "/Todo-list/static/js/main.84641061.chunk.js"
+  },
+  {
+    "revision": "895e78035450c62a9efe",
+    "url": "/Todo-list/static/js/runtime~main.9eebd9a7.js"
+  }
+]);
